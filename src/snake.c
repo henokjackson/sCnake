dc6331bcd94ad8fb739475b6753b5de26b438d2f
Henok Jackson <henokjackson@gmail.com>
2026-06-05 22:40:19 +0530

    Cleaned up comments

--- src/snake.c
+++ src/snake.c
@@ -49,11 +49,12 @@ void set_random_pos(
     const uint8_t permitted_coordinates_list_size,
     struct position* p_position) {
     /**
-     * INFO: While generating random coordinates there is a thing to consider. Generating a random coordinate by excluding some is
-     * very difficult. It involves using very clumsy logic to increment / decrement the generated random number to another one.
-     * There are a few practical ways to tackle this but they come with their own set of challenges:
+     * INFO:
+     *      While generating random coordinates there is a thing to consider. Generating a random coordinate by excluding some is
+     *      very difficult. It involves using very clumsy logic to increment / decrement the generated random number to another one.
+     *      There are a few practical ways to tackle this but they come with their own set of challenges:
      *
-     * -> 1. The first idea is to pass in a list of excluded coordinates. When we say coordinates it is a pair.
+     *  ->  The first idea is to pass in a list of excluded coordinates. When we say coordinates it is a pair.
      *      There are multiple ways to pass in the data. Either as a 2-D boolean array of size VIEWPORT_WIDTH x VIEWPORT_HEIGHT, where
      *      the element in the location [exclude_x][exlude_y] will be marked true, else false. But this would result in a sparse matrix,
      *      wasting stack space. Also using an array is restrictive as the size is fixed and the snake body size might not take up the whole
@@ -63,13 +64,13 @@ void set_random_pos(
      *      transformed into an array of the ones with only false elements which adds in more complexity. The only advantage here is the index.
      *      For this case, the time complexity would involve
      *
-     * -> 2. Second approach is to use a linked list this would take up lesser space as the linked list will take up the length of the snake
+     *  ->  Second approach is to use a linked list this would take up lesser space as the linked list will take up the length of the snake
      *      and the food coordinates. The issue here is randomization. As the nodes of a link list are not indexed it is difficult to access
      *      a particular element in O(1). However, using a generated random number, a for loop can be used to traverse the linked list. So the
      *      linked list must contain only the non-excluded ones. That makes it faster and smaller.
      *
-     * -> Conclusion: I don't understand why I even thought about the array idea. The coordinates to be excluded are of the snake and of the food
-     *    which are already a linked list. So, I'll go witht he linked list method.
+     *  ->  Conclusion: I don't understand why I even thought about the array idea. The coordinates to be excluded are of the snake and of the food
+     *      which are already a linked list. So, I'll go witht he linked list method.
      */
 
     // generate random index to select a coordinate from a list of permitted coordinates.
@@ -78,22 +79,23 @@ void set_random_pos(
     const unsigned int random_index = get_random_number(random_index_min, random_index_max, time(nullptr));
 
     const struct position* current_position_element = permitted_coordinates;
-    for (unsigned int i = 0; i == random_index ||  current_position_element -> next_element != nullptr; i++) {
-        current_position_element = current_position_element -> next_element;
-    }
+    for (unsigned int i = 0;
+        i < random_index || current_position_element->next_element != nullptr;
+         i++, current_position_element = current_position_element->next_element) {};
+
     // update position
     p_position -> pos_x = current_position_element -> pos_x;
     p_position -> pos_y = current_position_element -> pos_y;
 }
 
 void initialize_snake(struct snake* snake) {
-    snake->head = (struct snake_cell*)malloc(sizeof(struct snake_cell));
-    snake->tail = (struct snake_cell*)malloc(sizeof(struct snake_cell));
+    snake -> head = (struct snake_cell*)malloc(sizeof(struct snake_cell));
+    snake -> tail = (struct snake_cell*)malloc(sizeof(struct snake_cell));
 
-    snake->head->prev_cell = nullptr;
-    snake->head->next_cell = snake->tail;
+    snake -> head -> prev_cell = nullptr;
+    snake -> head -> next_cell = snake -> tail;
 
-    snake->tail->prev_cell = snake->head;
-    snake->tail->next_cell = nullptr;
+    snake -> tail -> prev_cell = snake -> head;
+    snake -> tail -> next_cell = nullptr;
 
 }
