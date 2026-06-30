e712f41e7c5fc08971deee54066c6983493e495d
Henok Jackson <henokjackson@gmail.com>
2026-07-01 00:08:49 +0530

    Added function to map snake head direction to pixel type.

--- src/snake.c
+++ src/snake.c
@@ -1,21 +1,21 @@
+#include <stdlib.h>
 #include "common.h"
 #include "food.h"
 #include "snake.h"
-#include <stdlib.h>
 
 void set_random_direction(direction* snake_direction) {
     const unsigned int random_number = get_random_number(1, sizeof(direction[0]), time(nullptr));
     *snake_direction = (direction)random_number;
 }
 
-position* generate_permitted_coordinates(const uint8_t snake_size, const uint8_t food_size) {
+position* generate_available_coordinates_list(const uint8_t snake_size, const uint8_t food_size) {
     /**
      * INFO:
      *  There is one thing that I missed out. Since, initial size of the snake will be 2,
      *  the snake head can only be placed outside the border as the tail has to be fit in the same row/column.
      *  Hence I need to write some logic to filter out the border pixels.
      *  The main issue here is that all of these depends on the initial direction chosen.
-     *  Hence the direction needs to be decided and then the list of permitted coordinates will be generated
+     *  Hence the direction needs to be decided and then the list of available coordinates will be generated
      *  based on that. The issue here is that I don't want the snake head and tail to be in two different places,
      *  which looks kinda weird. So the first thing is to bring determinsm by deciding the direction and then
      *  deciding the position based on the excluded one.
@@ -41,7 +41,7 @@ position* generate_permitted_coordinates(const uint8_t snake_size, const uint8_t
      *
      *  There's yet one another important think that I absolutely missed !
      *  The loop below just makes a list of coordinates of the snake body ! - why do I even need that ??!!
-     *  What I want is a list of permitted coordinates, coordinates that are not occupied by the snake body
+     *  What I want is a list of available coordinates, coordinates that are not occupied by the snake body
      *  or by the food or the ones that match the border condition depending on direction.
      *
      *  There are several approached poping up in my head !!
@@ -73,37 +73,42 @@ position* generate_permitted_coordinates(const uint8_t snake_size, const uint8_t
     /**
      * TODO: Don't forget to free this piece of shit !
      */
-    position* arr_permitted_coordinates = malloc((VIEWPORT_HEIGHT * VIEWPORT_WIDTH - snake_size - food_size) * sizeof(dll_position));
+    position* arr_available_coordinates = malloc((VIEWPORT_HEIGHT * VIEWPORT_WIDTH - snake_size - food_size) * sizeof(position));
 
     // Iterate the array and get a list of all the cells that are marked `false`.
     for (int i = 0; i < VIEWPORT_HEIGHT; i++) {
-        for (int j = 0; j < VIEWPORT_WIDTH; j++, arr_permitted_coordinates++) {
-            if (frame_buffer[i][j] == false) { // add a null pointer check here !
-                arr_permitted_coordinates -> x_coordinate = j;
-                arr_permitted_coordinates -> y_coordinate = i;
+        for (int j = 0; j < VIEWPORT_WIDTH; j++, arr_available_coordinates++) {
+            if (frame_buffer[i][j] == BLANK) { /** TODO: add a null pointer check here ! */
+                arr_available_coordinates -> x_coordinate = j;
+                arr_available_coordinates -> y_coordinate = i;
             }
         }
     }
 
-    return arr_permitted_coordinates;
+    return arr_available_coordinates;
 }
 
-void exclude_border_coordinates(const enum direction direction) {
+void set_excluded_border_coordinates(const enum direction direction) {
     for (int i = 0; i < VIEWPORT_HEIGHT; i++) {
         for (int j = 0; j < VIEWPORT_WIDTH; j++) {
             if ((direction == LEFT && j == 0) ||
                 (direction == DOWN && i == 0) ||
                 (direction == RIGHT && j == VIEWPORT_WIDTH - 1) ||
                 (direction == UP && i == VIEWPORT_HEIGHT - 1)) {
-                frame_buffer[i][j] = true;
+                frame_buffer[i][j] = BORDER;
             }
         }
     }
 }
 
-dll_position* get_snake_coordinates_list(const snake* p_snake) {
-    auto snake_coordinates_list = (dll_position*)malloc(sizeof(dll_position) * p_snake -> size);
-    for(dll_position* snake_cell_iterator = p_snake -> head;
+dll_pixel* get_snake_coordinates_list(const snake* p_snake) {
+    /**
+     * The elements in the linked list are stored in order. The first element is the head and tail is the
+     * element in the list.
+     */
+
+    auto snake_coordinates_list = (dll_pixel*)malloc(sizeof(dll_pixel) * p_snake -> size);
+    for(dll_pixel* snake_cell_iterator = p_snake -> head;
         snake_cell_iterator != nullptr;
         snake_cell_iterator = snake_cell_iterator -> next) {
         snake_coordinates_list -> position = snake_cell_iterator -> position;
@@ -112,6 +117,16 @@ dll_position* get_snake_coordinates_list(const snake* p_snake) {
     }
 
     return snake_coordinates_list;
+}
+
+void set_snake_head_pixel_type(const snake* p_snake) {
+    switch (p_snake -> direction) {
+        case LEFT: p_snake -> head -> type = SNAKE_HEAD_LEFT; break;
+        case RIGHT: p_snake -> head -> type = SNAKE_HEAD_RIGHT; break;
+        case UP: p_snake -> head -> type = SNAKE_HEAD_UP; break;
+        case DOWN: p_snake -> head -> type = SNAKE_HEAD_DOWN; break;
+        default: break;
+    }
 }
 
 void initialize_snake_and_food(snake* p_snake, const food* p_food) {
@@ -125,48 +140,60 @@ void initialize_snake_and_food(snake* p_snake, const food* p_food) {
     // initialize food size.
     const uint8_t food_size = p_food -> size;
 
-    // initialize permitted coordinates
-    position* arr_permitted_coordinates = nullptr;
+    // initialize available coordinates.
+    position* arr_available_coordinates = nullptr;
 
-    // initialize snake coordinates list
-    dll_position* snake_coordinates_list = nullptr;
+    // initialize snake coordinates list.
+    dll_pixel* snake_coordinates_list = nullptr;
 
+    /**
+     * TODO: Write a factory function that malloc()s the memory for snake and also
+     *       increments the size along with it to avoid mistakes.
+     */
     // create snake head.
-    p_snake -> head = (dll_position*)malloc(sizeof(dll_position));
+    p_snake -> head = (dll_pixel*)malloc(sizeof(dll_pixel));
+
+    // create snake tail.
+    p_snake -> tail = (dll_pixel*)malloc(sizeof(dll_pixel));
+
+    // set snake head parameters.
     p_snake -> head -> prev = nullptr;
     p_snake -> head -> next = p_snake -> tail;
     p_snake -> size++;
 
-    // create snake tail.
-    p_snake -> tail = (dll_position*)malloc(sizeof(dll_position));
+    // set snake tail parameters.
     p_snake -> tail -> prev = p_snake -> head;
     p_snake -> tail -> next = nullptr;
+    p_snake -> tail -> type = SNAKE_TAIL;
     p_snake -> size++;
 
-    // Calculate the initial position for head.
-    // generate list of permitted co-ordinates
-    arr_permitted_coordinates = generate_permitted_coordinates(p_snake -> size, food_size);
+    // Calculate the initial position for snake head.
+    // generate list of available co-ordinates
+    arr_available_coordinates = generate_available_coordinates_list(p_snake -> size, food_size);
 
     // set random initial position for snake head.
-    set_random_position(arr_permitted_coordinates, p_snake -> size, &p_snake -> head -> position);
+    set_random_position(arr_available_coordinates, p_snake -> size, &p_snake -> head -> position);
 
-    // set random initial direction.
+    // set random initial direction for snake head.
     set_random_direction(&p_snake -> direction);
 
-    // get list of coordinates of snake
+    // set pixel_type for snake head based on direction.
+    set_snake_head_pixel_type(p_snake);
+
+    // get list of coordinates of snake pixels.
     snake_coordinates_list = get_snake_coordinates_list(p_snake);
 
-    // render snake head on frame buffer
-    set_frame(snake_coordinates_list);
+    // set the snake head on frame buffer.
+    set_frame_buffer(snake_coordinates_list);
 
     // Calculate the initial position for tail using direction.
     // First, exclude the border coordinates.
-    exclude_border_coordinates(p_snake -> direction);
+    set_excluded_border_coordinates(p_snake -> direction);
 
     // Second, exclude the head coordinate.
     // Remove the pre-allocated coordinates list.
-    free((void*)arr_permitted_coordinates);
-    arr_permitted_coordinates = generate_permitted_coordinates(p_snake -> size, food_size);
+    free((void*)arr_available_coordinates);
+    arr_available_coordinates = generate_available_coordinates_list(p_snake -> size, food_size);
 
     set_p_snake -> tail -> position
 }
